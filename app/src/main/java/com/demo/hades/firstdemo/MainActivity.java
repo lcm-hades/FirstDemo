425193833566e15a2dc63ffa386d08268b9c1255
Hades <lcm_hades@163.com>
2016-08-15 12:57:40 +0800

    add TextView

--- app/src/main/java/com/demo/hades/firstdemo/MainActivity.java
+++ app/src/main/java/com/demo/hades/firstdemo/MainActivity.java
@@ -4,13 +4,18 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private TextView tv;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        tv = (TextView) findViewById(R.id.tv);
+        tv.setText("里约 2016");
     }
 
 }
