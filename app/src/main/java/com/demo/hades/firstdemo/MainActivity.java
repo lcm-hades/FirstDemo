fbcbcf9541b8278e24a67d94e57d79e481f06cad
Hades <lcm_hades@163.com>
2016-08-15 13:06:28 +0800

    change MainActivity

--- app/src/main/java/com/demo/hades/firstdemo/MainActivity.java
+++ app/src/main/java/com/demo/hades/firstdemo/MainActivity.java
@@ -1,5 +1,8 @@
 package com.demo.hades.firstdemo;
 
+import android.graphics.Canvas;
+import android.graphics.ColorFilter;
+import android.graphics.drawable.Drawable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
@@ -15,6 +18,27 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         tv = (TextView) findViewById(R.id.tv);
+        tv.setBackground(new Drawable() {
+            @Override
+            public void draw(Canvas canvas) {
+
+            }
+
+            @Override
+            public void setAlpha(int alpha) {
+
+            }
+
+            @Override
+            public void setColorFilter(ColorFilter colorFilter) {
+
+            }
+
+            @Override
+            public int getOpacity() {
+                return 0;
+            }
+        });
         tv.setText("里约 2016");
     }
 
